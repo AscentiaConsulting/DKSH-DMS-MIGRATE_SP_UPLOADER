0c540955fdc63fe4be2fba0e2edc02b602b0268e
Thanapol Sukhaya <Thanapol@ascentia-consulting.com>
2019-06-18 15:40:47 +0700

    initial

---
+++ SP_Uploader/SP_Uploader/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace SP_Uploader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbSharePointURL = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbDocLib = new System.Windows.Forms.TextBox();
+            this.btSubmit = new System.Windows.Forms.Button();
+            this.tbResult = new System.Windows.Forms.TextBox();
+            this.btCheckSource = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "SharePoint URL";
+            // 
+            // tbSharePointURL
+            // 
+            this.tbSharePointURL.Location = new System.Drawing.Point(123, 10);
+            this.tbSharePointURL.Name = "tbSharePointURL";
+            this.tbSharePointURL.Size = new System.Drawing.Size(353, 20);
+            this.tbSharePointURL.TabIndex = 1;
+            this.tbSharePointURL.Text = "https://dksh.sharepoint.com/sites/QAHECDMS";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Document Library";
+            // 
+            // tbDocLib
+            // 
+            this.tbDocLib.Location = new System.Drawing.Point(123, 38);
+            this.tbDocLib.Name = "tbDocLib";
+            this.tbDocLib.Size = new System.Drawing.Size(353, 20);
+            this.tbDocLib.TabIndex = 3;
+            this.tbDocLib.Text = "CUSTOMER_MIG";
+            // 
+            // btSubmit
+            // 
+            this.btSubmit.Location = new System.Drawing.Point(493, 273);
+            this.btSubmit.Name = "btSubmit";
+            this.btSubmit.Size = new System.Drawing.Size(88, 23);
+            this.btSubmit.TabIndex = 4;
+            this.btSubmit.Text = "Execute";
+            this.btSubmit.UseVisualStyleBackColor = true;
+            this.btSubmit.Click += new System.EventHandler(this.btSubmit_Click);
+            // 
+            // tbResult
+            // 
+            this.tbResult.Enabled = false;
+            this.tbResult.Location = new System.Drawing.Point(12, 74);
+            this.tbResult.Multiline = true;
+            this.tbResult.Name = "tbResult";
+            this.tbResult.Size = new System.Drawing.Size(460, 222);
+            this.tbResult.TabIndex = 5;
+            // 
+            // btCheckSource
+            // 
+            this.btCheckSource.Location = new System.Drawing.Point(493, 72);
+            this.btCheckSource.Name = "btCheckSource";
+            this.btCheckSource.Size = new System.Drawing.Size(88, 23);
+            this.btCheckSource.TabIndex = 6;
+            this.btCheckSource.Text = "Check Source";
+            this.btCheckSource.UseVisualStyleBackColor = true;
+            this.btCheckSource.Click += new System.EventHandler(this.btCheckSource_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(593, 327);
+            this.Controls.Add(this.btCheckSource);
+            this.Controls.Add(this.tbResult);
+            this.Controls.Add(this.btSubmit);
+            this.Controls.Add(this.tbDocLib);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbSharePointURL);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "DMS SharePoint Uploader Tool";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbSharePointURL;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbDocLib;
+        private System.Windows.Forms.Button btSubmit;
+        private System.Windows.Forms.TextBox tbResult;
+        private System.Windows.Forms.Button btCheckSource;
+    }
+}
+
---
+++ SP_Uploader/SP_Uploader/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.SharePoint.Client;
+
+namespace SP_Uploader
+{
+    public partial class Form1 : System.Windows.Forms.Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void CreateDocumentLibrary(string doclibName)
+        {
+            string url = tbSharePointURL.Text;
+            string username = "dms_admin.th@dksh.com";
+            string password = "Dksh1234";
+            OfficeDevPnP.Core.AuthenticationManager authMgr = new OfficeDevPnP.Core.AuthenticationManager();
+            var docLib = new ListCreationInformation()
+            {
+                Title = doclibName,
+                Description = "system created document Library for storing DKSH documents",
+                TemplateType = 101 //document library
+            };
+            using (var ctx = authMgr.GetSharePointOnlineAuthenticatedContextTenant(url, username, password))
+            {
+                if (!ctx.Site.RootWeb.ListExists(doclibName))
+                {
+                    ctx.Web.Lists.Add(docLib);
+                    ctx.ExecuteQuery();
+                }
+            }
+        }
+        private void btCheckSource_Click(object sender, EventArgs e)
+        {
+            using (SourceEntities db = new SourceEntities())
+            {
+                List<UPLOADER> list = db.UPLOADERs.Where(item => item.PROCESS_FLAG == true && item.FILEPATH != null && item.FILEPATH != "").ToList();
+                tbResult.Text += string.Format("Total valid records for processing: {0}\r\n", list.Count);
+            }
+        }
+        public string UploadDocuments(string filePath)
+        {
+            try
+            {
+                string url = tbSharePointURL.Text;
+                string username = "dms_admin.th@dksh.com";
+                string password = "Dksh1234";
+                string docLib = tbDocLib.Text;
+                // CreateDocumentLibrary(docLib);
+                //CreateFolderinDocumentLibrary(docLib, processID);
+                //string serverRelativeURL = string.Format("{0}/{1}", docLib, processID);
+                OfficeDevPnP.Core.AuthenticationManager authMgr = new OfficeDevPnP.Core.AuthenticationManager();
+                string fileName = Path.GetFileName(filePath);
+                using (var ctx = authMgr.GetSharePointOnlineAuthenticatedContextTenant(url, username, password))
+                {
+                    Folder folder = ctx.Web.GetFolderByServerRelativeUrl(docLib);
+                    folder.UploadFile(fileName, filePath, true);
+                }
+                return string.Format("{0}/{1}/{2}", url, docLib, fileName);
+            }
+            catch (Exception ex)
+            {
+                return ex.ToString();
+            }
+
+        }
+
+        private void btSubmit_Click(object sender, EventArgs e)
+        {
+            CreateDocumentLibrary(tbDocLib.Text);
+            tbResult.Text = ">>Start process.\r\n";
+            bool isconnect = CheckConnection();
+            using (SourceEntities db = new SourceEntities())
+            {
+                List<UPLOADER> list = db.UPLOADERs.Where(item => item.PROCESS_FLAG == true && item.FILEPATH != null && item.FILEPATH != "").ToList();
+                tbResult.Text += string.Format("Total valid records for processing: {0}\r\n", list.Count);
+                foreach (UPLOADER file in list)
+                {
+                    string result = UploadDocuments(file.FILEPATH);
+                    bool success = false;
+                    if (result.StartsWith("https"))
+                    {
+                        file.PROCESS_FLAG = false;
+                        file.PROCESSEDTIME = DateTime.Now;
+                        file.RESULT = "OK";
+                        file.UPLOADEDURL = result;
+                        tbResult.Text += string.Format("DocID: {0}, success\r\n", file.DOCID);
+                        success = true;
+                    }
+                    else
+                    {
+                        file.RESULT = "ERROR";
+                        file.ERRORMESSAGE = result;
+                        tbResult.Text += string.Format("DocID: {0}, fail\r\n", file.DOCID);
+                    }
+                    db.SaveChanges();
+                    if (success && isconnect)
+                    {
+                        using (DestinationEntities db2 = new DestinationEntities())
+                        {
+                            T_STG_DOC doc = db2.T_STG_DOC.FirstOrDefault(item => item.DocID == file.DOCID);
+                            if (doc != null)
+                            {
+                                doc.DOCURL = file.UPLOADEDURL;
+                                file.ISUPDATEDSTAGING = true;
+                                file.STAGINGUPDATEDTIME = DateTime.Now;
+                            }
+                            else
+                            {
+                                file.ERRORMESSAGE = "cannot find record to update";
+                            }
+                            db.SaveChanges();
+                            db2.SaveChanges();
+                        }
+                    }
+                }
+            }
+            tbResult.Text += ">>Finish process.\n";
+        }
+        public bool CheckConnection()
+        {
+            try
+            {
+                using (DestinationEntities db2 = new DestinationEntities())
+                {
+                    T_STG_DOC doc = db2.T_STG_DOC.FirstOrDefault(item => item.DocID == "");
+                }
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
